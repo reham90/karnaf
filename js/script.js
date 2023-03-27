1ae084e7230d6c8812be4a06d393ed4115b8683d
reham90 <rehamadel344@gmail.com>
2023-03-27 16:24:07 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -377,9 +377,22 @@ function clickOtherK() {
  document.getElementById('other-k').focus();
 }
 
-$(function(){
-	window.myFlux = new flux.slider('#slider', {
-		autoplay: false,
-		pagination: true
+// $(function(){
+// 	window.myFlux = new flux.slider('#slider', {
+// 		autoplay: false,
+// 		pagination: true
+// 	});
+// });
+
+$(document).ready(function() {
+ 	$("#css3dimagePager li").click(function(){
+		var rotateY = ($(this).index() * -90); 
+		$("#css3dimageslider ul").css({"-webkit-transform":"rotateY("+rotateY+"deg)", "-moz-transform":"rotateY("+rotateY+"deg)", "-ms-transform":"rotateY("+rotateY+"deg)", "transform":"rotateY("+rotateY+"deg)"});
+		$("#css3dimagePager li").removeClass("active");
+		$(this).addClass("active");
 	});
-});
+ 	$("#css3dtransparency").click(function() {
+		$("#css3dimageslider").toggleClass("transparency");
+		$(this).toggleClass("active");
+	});
+ });
