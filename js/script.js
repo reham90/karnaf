e2a392fe2d9a907ccbebb124c2d3e5b4171e2d43
reham90 <rehamadel344@gmail.com>
2023-03-27 18:58:23 +0300

    edit

--- js/script.js
+++ js/script.js
@@ -386,10 +386,12 @@ function clickOtherK() {
 
 $(document).ready(function() {
  	$("#css3dimagePager li").click(function(){
+    // $('#css3dimageslider').blindify();
 		var rotateY = ($(this).index() * -90); 
-		$("#css3dimageslider ul").css({"-webkit-transform":"rotateY("+rotateY+"deg)", "-moz-transform":"rotateY("+rotateY+"deg)", "-ms-transform":"rotateY("+rotateY+"deg)", "transform":"rotateY("+rotateY+"deg)"});
+		$("#css3dimageslider ul ").css({"-webkit-transform":"rotateY("+rotateY+"deg)", "-moz-transform":"rotateY("+rotateY+"deg)", "-ms-transform":"rotateY("+rotateY+"deg)", "transform":"rotateY("+rotateY+"deg)"});
 		$("#css3dimagePager li").removeClass("active");
 		$(this).addClass("active");
+    
 	});
  	$("#css3dtransparency").click(function() {
 		$("#css3dimageslider").toggleClass("transparency");
